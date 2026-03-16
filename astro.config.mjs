b3995a4ed3077a6bf18c7e8c722bac90c4e43fbd
Nachin <ruben.godoy@ngg.cl>
2026-03-16 17:58:32 -0300

    S

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // <--- ESTO ES VITAL para que el login y la API funcionen
+  security: {
+    checkOrigin: false,
+  },
   vite: {
     plugins: [tailwindcss()]
   },
