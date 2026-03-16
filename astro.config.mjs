1f4aacedd35d38746c32d990e6110bc5cfbc4659
Nachin <ruben.godoy@ngg.cl>
2026-03-16 18:53:07 -0300

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // <--- ESTO ES VITAL para que el login y la API funcionen
+  server: {
+    port: 4321,
+    host: true, // Esto es VITAL: permite conexiones externas al contenedor
+  },
   security: {
     checkOrigin: false,
   },
